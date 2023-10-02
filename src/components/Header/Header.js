b26f88adb0592f7989786a6ae5f2968c71ca63ff
Sajidwebsite <msjdkhan9@gmail.com>
2023-10-02 11:37:16 +0600

    Header-complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from '../../Images/Logo.png'
+import logo from '../../Images/Logo.png';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <div>
-                <img src={logo} alt="" />
-            </div>
+        <div className="flex items-center justify-around w-4/5 mx-auto">
             <div  >
+                <img className="w-3/4 mr-5" src={logo} alt="" />
+            </div>
+
+            <div  className="">
             <a href="">Online Consultaion</a>
              <a href="">Treatments</a>
              <a href="">Testimonials</a>
              <a href="">Blogs</a>
 
-             <div>
+             
             <a href="">Language</a>
              <span>|</span>
              <a href="">Login</a>
-             </div>
+             
 
              <a href="">Get Medical Advice</a>
             </div>
